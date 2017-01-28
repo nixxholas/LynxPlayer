4fe338a2cf8131d367614d1de894a6557a213a64
nixxholas <nixholas@outlook.com>
2017-01-28 12:15:52 +0800

    Source: Custom Font Implementation
    Signed-off-by: nixxholas <nixholas@outlook.com>

---
+++ app/src/main/java/com/nixholas/materialtunes/UI/Text/RegularTextView.java
@@ -0,0 +1,40 @@
+package com.nixholas.materialtunes.UI.Text;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by nixholas on 28/1/17.
+ *
+ * http://stackoverflow.com/questions/27588965/how-to-use-custom-font-in-android-studio
+ */
+
+public class RegularTextView extends TextView {
+
+    public RegularTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    public RegularTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public RegularTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public RegularTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Montserrat-Regular.ttf");
+        setTypeface(tf);
+    }
+}
