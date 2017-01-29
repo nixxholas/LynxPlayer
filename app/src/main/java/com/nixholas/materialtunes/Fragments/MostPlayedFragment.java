775bae6a87200fa026bc7c41699608d08d5a8770
nixxholas <nixholas@outlook.com>
2017-01-29 15:06:49 +0800

    TopPlayed: Initial Implementation
    The adapter for TopPlayed is almost done, we're just missing some touch up here and there followed by a placeholder for the fragment.
    Will now have to head back to MediaDB to make sure that we'll be returning the play count as well to update the songs.
    
    Signed-off-by: nixxholas <nixholas@outlook.com>

---
+++ app/src/main/java/com/nixholas/materialtunes/Fragments/MostPlayedFragment.java
@@ -0,0 +1,8 @@
+package com.nixholas.materialtunes.Fragments;
+
+/**
+ * Created by nixholas on 29/1/17.
+ */
+
+public class MostPlayedFragment {
+}
---
+++ app/src/main/java/com/nixholas/materialtunes/Media/Adapter/TopPlayedAdapter.java
@@ -0,0 +1,80 @@
+package com.nixholas.materialtunes.Media.Adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.nixholas.materialtunes.Media.Entities.Song;
+import com.nixholas.materialtunes.R;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nixholas on 29/1/17.
+ */
+
+public class TopPlayedAdapter extends RecyclerView.Adapter<TopPlayedAdapter.ViewHolder> {
+    private ArrayList<Song> mDataset;
+    private Context context;
+    int color, textColor;
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public View v;
+        TextView title, count;
+        RelativeLayout currentLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.currentLayout = (RelativeLayout) v.findViewById(R.id.topplayeditem_layout);
+            this.title = (TextView) v.findViewById(R.id.topplayeditem_title);
+            this.count = (TextView) v.findViewById(R.id.topplayeditem_count);
+        }
+    }
+
+    public TopPlayedAdapter(ArrayList<Song> mDataset, int color, int textColor) {
+        this.mDataset = mDataset;
+        this.color = color;
+        this.textColor = textColor;
+    }
+
+    @Override
+    public TopPlayedAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Create a new view
+        final View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.topplayed_item, parent, false);
+
+        // Set the view's size, margins, paddings and layout parameters
+        TopPlayedAdapter.ViewHolder vh = new TopPlayedAdapter.ViewHolder(v);
+
+        // http://stackoverflow.com/questions/32136973/how-to-get-a-context-in-a-recycler-view-adapter
+        context = parent.getContext();
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(TopPlayedAdapter.ViewHolder holder, int position) {
+        final Song currentSong = mDataset.get(position);
+
+        holder.title.setText(currentSong.getTitle());
+        holder.count.setText(currentSong.getCount() + "");
+
+        holder.currentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
