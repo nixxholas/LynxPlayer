7b6fcf7bd8dfe7fedcbeae46e1f562295b992cfa
nixxholas <nixholas@outlook.com>
2017-03-04 02:40:38 +0800

    Source: Studio 2.3.0 Compatibility Update
    Signed-off-by: nixxholas <nixholas@outlook.com>

--- app/src/main/java/com/nixholas/materialtunes/MainActivity.java
+++ app/src/main/java/com/nixholas/materialtunes/MainActivity.java
@@ -271,6 +271,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Home");
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
