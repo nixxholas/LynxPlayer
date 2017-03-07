4781af5858e028eb0606cb8cfb805e4e96546f2f
nixxholas <nixholas@outlook.com>
2017-03-07 22:18:12 +0800

    UI: MediaControlUpdater: Optimize play and pause
    Signed-off-by: nixxholas <nixholas@outlook.com>

--- app/src/main/java/com/nixholas/materialtunes/UI/MediaControlUpdater.java
+++ app/src/main/java/com/nixholas/materialtunes/UI/MediaControlUpdater.java
@@ -36,8 +36,7 @@ public class MediaControlUpdater {
     public static void mediaControlsOnClickPlayPause() {
         Log.d("mCOnClickPlayPause", "Running method");
 
-        if (mediaManager.mMediaPlayer != null) {
-            if (mediaManager.mMediaPlayer.isPlaying() || mediaManager.mediaPlayerIsPaused) {
+        if (mediaManager.mMediaPlayer != null && mediaManager.mMediaPlayer.isPlaying()) {
                 // http://stackoverflow.com/questions/25381624/possible-to-detect-paused-state-of-mediaplayer
                 if (mediaManager.mediaPlayerIsPaused) { // If the current song is paused,
                     mediaManager.mMediaPlayer.start();
@@ -53,7 +52,6 @@ public class MediaControlUpdater {
                 }
 
                 persistentNotif.updateNotification();
-            }
         }
     }
 
